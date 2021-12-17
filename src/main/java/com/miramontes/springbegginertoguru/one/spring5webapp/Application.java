43f5b25a89f7890c746c3133451ab48883994cec
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-17 13:42:20 -0600

    init commit

---
+++ src/main/java/com/miramontes/springbegginertoguru/one/spring5webapp/Application.java
@@ -0,0 +1,13 @@
+package com.miramontes.springbegginertoguru.one.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
