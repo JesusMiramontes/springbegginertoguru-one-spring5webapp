d27c406804d5844a4232275ea6b6b7e2c9e3f69d
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-17 15:50:57 -0600

    Bootstrap publishers

--- src/main/java/com/miramontes/springbegginertoguru/one/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/com/miramontes/springbegginertoguru/one/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.miramontes.springbegginertoguru.one.spring5webapp.bootstrap;
 
 import com.miramontes.springbegginertoguru.one.spring5webapp.model.Author;
 import com.miramontes.springbegginertoguru.one.spring5webapp.model.Book;
+import com.miramontes.springbegginertoguru.one.spring5webapp.model.Publisher;
 import com.miramontes.springbegginertoguru.one.spring5webapp.repositories.AuthorRepository;
 import com.miramontes.springbegginertoguru.one.spring5webapp.repositories.BookRepository;
+import com.miramontes.springbegginertoguru.one.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -13,30 +15,45 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
     // Will do dependency injection and inject an instance of author repository and book repository.
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher p1 = new Publisher("Name1", "Address1", "City1", "State1", "zip1");
+
         Author a1 = new Author("Name1", "LastName1");
         Book b1 = new Book("Title1", "isbn1");
         a1.getBooks().add(b1);
         b1.getAuthors().add(a1);
 
+        b1.setPublisher(p1);
+        p1.getBooks().add(b1);
+
         authorRepository.save(a1);
         bookRepository.save(b1);
+        publisherRepository.save(p1);
+
+
+        Publisher p2 = new Publisher("Name2", "Address2", "City2", "State2", "zip2");
 
         Author a2 = new Author("Name2", "LastName2");
         Book b2 = new Book("Title2", "isbn2");
         a2.getBooks().add(b2);
         b2.getAuthors().add(a2);
 
+        b2.setPublisher(p2);
+        p2.getBooks().add(b2);
+
         authorRepository.save(a2);
         bookRepository.save(b2);
+        publisherRepository.save(p2);
 
         System.out.println("bookRepository.count() = " + bookRepository.count());
     }
