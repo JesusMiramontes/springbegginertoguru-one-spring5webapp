e91708884ff274373305b34ccb4d90c6274b2d9d
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-17 15:08:04 -0600

    Created Publisher POJO

---
+++ src/main/java/com/miramontes/springbegginertoguru/one/spring5webapp/model/Publisher.java
@@ -0,0 +1,29 @@
+package com.miramontes.springbegginertoguru.one.spring5webapp.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@EqualsAndHashCode(exclude = "books")
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String addressLine1;
+    private String city;
+    private String state;
+    private String zip;
+
+    private Set<Book> books = new HashSet<>();
+}
