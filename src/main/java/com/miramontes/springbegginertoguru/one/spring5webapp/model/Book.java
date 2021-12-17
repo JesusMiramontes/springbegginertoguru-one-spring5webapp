f73f34ef58f119c2bb6fc1ae2ef1716e5241e9c0
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-17 14:11:02 -0600

    Converted into JPA Entities

--- src/main/java/com/miramontes/springbegginertoguru/one/spring5webapp/model/Book.java
+++ src/main/java/com/miramontes/springbegginertoguru/one/spring5webapp/model/Book.java
@@ -2,10 +2,19 @@ package com.miramontes.springbegginertoguru.one.spring5webapp.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
 @Data
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
     private Set<Author> authors;
